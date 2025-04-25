e0c7de900c2dfe81d1f890fa5903c4e8d509820c
Ondřej Čopák <ondra.copak@gmail.com>
2025-04-25 23:31:48 +0200

    created + implemented virtual input periphs

--- src/periphs_virtual/input.cpp
+++ src/periphs_virtual/input.cpp
@@ -1,1 +1,64 @@
-#include "input.h"
+#include "input.hpp"
+
+#include <iostream>
+#include <unistd.h>
+
+#define fsizeRGB 3
+#define fsizePRESSES 1
+
+InputPeripherals::InputPeripherals() {}
+
+// Returns the difference between the current and previous state of the RGB knobs
+InputDelta InputPeripherals::get_delta() {
+	FILE *fp = fopen(fname_knobs.c_str(), "rb");
+	if (!fp) {
+		std::cerr << "Error opening file: " << fname_knobs << std::endl;
+		return {0, 0, 0};
+	}
+
+	uint32_t rgb_values = 0;
+	if (fread(&rgb_values, fsizeRGB, 1, fp) != 1) {
+		std::cerr << "Error reading file: " << fname_knobs << std::endl;
+		fclose(fp);
+		return {0, 0, 0};
+	}
+	fclose(fp);
+
+	uint8_t red = (rgb_values >> 16) & 0xFF;
+	uint8_t green = (rgb_values >> 8) & 0xFF;
+	uint8_t blue = rgb_values & 0xFF;
+
+	if (previous_delta_initialized == false) {
+		previous_delta = {red, green, blue};
+		previous_delta_initialized = true;
+		return {0, 0, 0};
+	}
+
+	// numbers can overflow, which is good, and we will use it to
+	// result in the closest difference of the circle knobs.
+	int8_t red_delta = (red - previous_delta.red);
+	int8_t green_delta = (green - previous_delta.green);
+	int8_t blue_delta = (blue - previous_delta.blue);
+
+	previous_delta = {red, green, blue};
+	return {red_delta, green_delta, blue_delta};
+}
+
+// Returns the current state of the RGB knobs
+InputPresses InputPeripherals::input_presses() {
+	FILE *fp = fopen(fname_knob_presses.c_str(), "rb");
+	if (fp == NULL) {
+		std::cerr << "Error opening file: " << fname_knob_presses << std::endl;
+		return {false, false, false, false};
+	}
+	char presses_values = 0;
+	if (fread(&presses_values, fsizePRESSES, 1, fp) != 1) {
+		std::cerr << "Error reading file a: " << fname_knobs << std::endl;
+		fclose(fp);
+		return {false, false, false, false};
+	}
+	bool red = (presses_values >> 0) & 0x01;
+	bool green = (presses_values >> 1) & 0x01;
+	bool blue = (presses_values >> 2) & 0x01;
+	return {red, green, blue, (red || green || blue)};
+}
---
+++ src/periphs_virtual/input.hpp
@@ -0,0 +1,41 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+#include <cstdint>
+#include <string>
+
+struct InputDelta {
+	int8_t red;
+	int8_t green;
+	int8_t blue;
+};
+
+struct InputNow {
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+};
+
+struct InputPresses {
+	bool red;
+	bool green;
+	bool blue;
+	bool any;
+};
+
+class InputPeripherals {
+	private:
+		bool previous_delta_initialized = false;
+		std::string fname_knobs = "src/periphs_virtual/memory/knobs.in";
+		std::string fname_knob_presses = "src/periphs_virtual/memory/knob_presses.in";
+		InputNow previous_delta;
+
+	public:
+		InputPeripherals();
+		InputDelta get_delta();
+		InputPresses input_presses();
+};
+
+
+
+#endif // INPUT_HPP
