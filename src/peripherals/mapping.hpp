dbffec3192dccf08475933ba4d9e8387f557d360
Šimon Brandner <simon.bra.ag@gmail.com>
2025-04-29 11:00:25 +0200

    Fix formatting
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/peripherals/mapping.hpp
+++ src/peripherals/mapping.hpp
@@ -4,15 +4,15 @@
 #include <cstdint>
 
 class PeripheralMemoryMapping {
-  private:
-	const uint8_t *lcd_address;
-	const uint8_t *spi_address;
+	private:
+		const uint8_t *lcd_address;
+		const uint8_t *spi_address;
 
-  public:
-	PeripheralMemoryMapping();
+	public:
+		PeripheralMemoryMapping();
 
-	const uint8_t *const get_lcd_address() const;
-	const uint8_t *const get_spi_address() const;
+		const uint8_t *const get_lcd_address() const;
+		const uint8_t *const get_spi_address() const;
 };
 
 #endif // MAPPING_H
