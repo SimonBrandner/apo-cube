4ee3674e9fbfaff2f3bd5de17b15b43d30c91ac8
Ondřej Čopák <ondra.copak@gmail.com>
2025-04-30 22:56:42 +0200

    brutal optimization of filling sides, about 5x faster, removed multiple if statements and nested for-loops

--- src/render/transform_2d.cpp
+++ src/render/transform_2d.cpp
@@ -1,22 +1,19 @@
 #include "transform_2d.hpp"
 #include <array>
 
-Vector rescale_2d_to_screen(Vector point2d) {
-	int width = SCREEN_WIDTH;
-	int height = SCREEN_HEIGHT;
+#define FOV 90.0f
 
+Vector rescale_2d_to_screen(Vector point2d) {
 	float x_screen;
 	float y_screen;
 
-	if (width >= height) {
-		x_screen = (point2d.get_x() + 1.0f) * 0.5f * height + (width - height) * 0.5f;
-		y_screen = (1.0f - point2d.get_y()) * 0.5f * height;
+	if (SCREEN_WIDTH >= SCREEN_HEIGHT) {
+		x_screen = (point2d.get_x() + 1.0f) * 0.5f * SCREEN_HEIGHT + (SCREEN_WIDTH - SCREEN_HEIGHT) * 0.5f;
+		y_screen = (1.0f - point2d.get_y()) * 0.5f * SCREEN_HEIGHT;
 	} else {
-		x_screen = (point2d.get_x() + 1.0f) * 0.5f * width;
-		y_screen = (1.0f - point2d.get_y()) * 0.5f * width + (height - width) * 0.5f;
+		x_screen = (point2d.get_x() + 1.0f) * 0.5f * SCREEN_WIDTH;
+		y_screen = (1.0f - point2d.get_y()) * 0.5f * SCREEN_WIDTH + (SCREEN_HEIGHT - SCREEN_WIDTH) * 0.5f;
 	}
-	//x_screen = (point2d.get_x() + 1.0f) * 0.5f * width;
-	//y_screen = (1.0f - point2d.get_y()) * 0.5f * height;
 
 	return Vector(x_screen, y_screen, point2d.get_z());
 }
@@ -31,10 +28,9 @@ Vector convert_to_2d(Vector point) {
 	}
 
 	// prerspective projection
-	const float FOV = 90.0f; // fov deg, now const, in the future parameter maybe
 
 	float scale = tan_deg(FOV * 0.5f);
-	float x_ndc = (x / z) * scale;
+	float x_ndc = (x / z) * scale; // normalized device coordinates
 	float y_ndc = (y / z) * scale;
 
 	Vector rescaled = Vector(x_ndc, y_ndc, z); // z will be used for z-buffer
@@ -107,73 +103,78 @@ void calculate_pixels_bresenham(std::array<Vector, 4> corners, Color color,
 	fill_side(corners, color, pixels, z_buffer, is_pixel);
 }
 
+// fills the sides with the square by lines
 void fill_side(std::array<Vector, 4> corners, Color color,
 								Color pixels[SCREEN_HEIGHT][SCREEN_WIDTH],
 								float z_buffer[SCREEN_HEIGHT][SCREEN_WIDTH],
 								bool is_pixel[SCREEN_HEIGHT][SCREEN_WIDTH]) {
-	int min_x = SCREEN_WIDTH;
-	int min_y = SCREEN_HEIGHT;
-	int max_x = 0;
-	int max_y = 0;
 
-	for (size_t i = 0; i < corners.size(); ++i) {
-		if (corners[i].get_x() < min_x) {
-			min_x = static_cast<int>(corners[i].get_x());
-		}
-		if (corners[i].get_y() < min_y) {
-			min_y = static_cast<int>(corners[i].get_y());
-		}
-		if (corners[i].get_x() > max_x) {
-			max_x = static_cast<int>(corners[i].get_x());
-		}
-		if (corners[i].get_y() > max_y) {
-			max_y = static_cast<int>(corners[i].get_y());
-		}
+	int min_x = SCREEN_WIDTH, min_y = SCREEN_HEIGHT;
+	int max_x = 0, max_y = 0;
+
+	// find the bounding box of the square, so all the vertices are inside the box
+	for (Vector& corner : corners) {
+		int x = static_cast<int>(corner.get_x());
+		int y = static_cast<int>(corner.get_y());
+		min_x = std::min(min_x, x);
+		min_y = std::min(min_y, y);
+		max_x = std::max(max_x, x);
+		max_y = std::max(max_y, y);
 	}
 
+	// making sure the box is inside the screen
 	min_x = std::max(min_x-1, 0);
 	min_y = std::max(min_y-1, 0);
 	max_x = std::min(max_x+1, SCREEN_WIDTH-1);
 	max_y = std::min(max_y+1, SCREEN_HEIGHT-1);
 
+	// fill the pixels inside the bounding box
 	for (int y = min_y; y <= max_y; ++y) {
 		bool is_inside = false;
-		float z_index_first = 0;
-		float z_index_last = 0;
-		long diff_first_last = 0;
-		long cur_index = 0;
+		float z_start = 0.0f;
+		float z_end = 0.0f;
+		int span_length = 0;
 
+		// get the z values of the pixels and length in the given line
 		for (int x = min_x; x <= max_x; ++x) {
 			if (is_pixel[y][x]) {
-				if (not is_inside) {
-					z_index_first = z_buffer[y][x];
+				if (!is_inside) {
+					z_start = z_buffer[y][x];
 				}
-				is_inside = not is_inside;
+				is_inside = !is_inside;
+
 				if (is_inside) {
-					++diff_first_last;
-					z_index_last = z_buffer[y][x];
+					++span_length;
+					z_end = z_buffer[y][x];
 				}
 			}
 		}
 
+		// if in the current row left and right square side pixels are the same
 		if (is_inside == true) {
-			diff_first_last = 1;
-			z_index_last = z_index_first;
+			span_length = 1;
+			z_end = z_start;
 		}
-		is_inside = false;
 
+		is_inside = false;
+		int fill_index = 0;
+
+		// fill the pixels in the given line in-between the square side pixels
 		for (int x = min_x; x <= max_x; ++x) {
 			if (is_pixel[y][x]) {
-				is_inside = not is_inside;
+				is_inside = !is_inside;
 			}
-			if (is_inside) {
-				if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
-					float z = z_index_first + (z_index_last-z_index_first) * ((float)cur_index / (float)diff_first_last);
-					++cur_index;
-					if (z > z_buffer[y][x]) {
-						z_buffer[y][x] = z;
-						pixels[y][x] = color;
-					}
+
+			if (is_inside && x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+				// linear interpolation of z
+				float z = z_start + (z_end - z_start) * (float)((fill_index) / std::max(1, span_length));
+
+				++fill_index;
+
+				// check if the pixel is visible
+				if (z > z_buffer[y][x]) {
+					z_buffer[y][x] = z;
+					pixels[y][x] = color;
 				}
 			}
 		}
