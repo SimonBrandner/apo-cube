aad6075719fcfe9113fc0447d392b948b7344688
Ondřej Čopák <ondra.copak@gmail.com>
2025-04-30 11:17:48 +0200

    grid line cube

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -161,7 +161,7 @@ void calculate_pixels_bresenham(std::array<Vector, 4> corners, Color color,
 	}
 
 	// now get the top left corner of the corners and the bottom right corner
-	int min_x = SCREEN_WIDTH;
+	/*int min_x = SCREEN_WIDTH;
 	int min_y = SCREEN_HEIGHT;
 	int max_x = 0;
 	int max_y = 0;
@@ -223,6 +223,6 @@ void calculate_pixels_bresenham(std::array<Vector, 4> corners, Color color,
 				}
 			}
 		}
-	}
+	}*/
 
 }
