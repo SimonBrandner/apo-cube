2b1c5a8110d1106bea5487a3176a81be738cd480
Ondřej Čopák <ondra.copak@gmail.com>
2025-05-05 21:52:26 +0200

    sin cos micro optimization

--- src/render/transform_3d.cpp
+++ src/render/transform_3d.cpp
@@ -9,13 +9,18 @@ void transform_vector(Camera camera, Vector &point) {
 	float yaw = camera.get_yaw();
 	float pitch = camera.get_pitch();
 
-	float yaw_data[9] = {cos_deg(yaw), 0, -sin_deg(yaw),
+	float sin_yaw = sin_deg(yaw);
+	float cos_yaw = cos_deg(yaw);
+	float sin_pitch = sin_deg(pitch);
+	float cos_pitch = cos_deg(pitch);
+
+	float yaw_data[9] = {cos_yaw, 0, -sin_yaw,
 		                 0, 1, 0,
-						 sin_deg(yaw), 0, cos_deg(yaw)};
+						 sin_yaw, 0, cos_yaw};
 
 	float pitch_data[9] = {1, 0, 0,
-						   0, cos_deg(pitch), -sin_deg(pitch),
-						   0, sin_deg(pitch), cos_deg(pitch)};
+						   0, cos_pitch, -sin_pitch,
+						   0, sin_pitch, cos_pitch};
 
 	Matrix yaw_matrix = Matrix(yaw_data);
 	Matrix pitch_matrix = Matrix(pitch_data);
